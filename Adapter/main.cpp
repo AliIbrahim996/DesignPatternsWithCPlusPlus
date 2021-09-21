2a65cb94e014607bc657435610d2982be1f5c565
Ali Ibrahim <allosheribraheem38@gmail.com>
2021-09-21 16:40:22 +0300

    use make_unique insted of new

--- Adapter/main.cpp
+++ Adapter/main.cpp
@@ -5,9 +5,9 @@ int main(int argc, char** argv)
 	std::string img = "https://../../demo.png";
 	std::string text = "hello";
 	std::string video = "https://../../demo.mp4";
-	std::unique_ptr<Adapter::Instagram> inst;
-	std::unique_ptr<Adapter::SocialAdapter> fb_adapter(new Adapter::FacebookAdapter());
-	std::unique_ptr<Adapter::SocialAdapter> inst_adapter(new Adapter::InstagramAdapter(inst));
+	std::unique_ptr<Adapter::Instagram> inst = std::make_unique<Adapter::Instagram>();
+	std::unique_ptr<Adapter::SocialAdapter> fb_adapter = std::make_unique<Adapter::FacebookAdapter>();
+	std::unique_ptr<Adapter::SocialAdapter> inst_adapter = std::make_unique<Adapter::InstagramAdapter>(inst);
 	std::list<std::unique_ptr<Adapter::SocialAdapter>> adapters;
 	adapters.push_back(std::move(fb_adapter));
 	adapters.push_back(std::move(inst_adapter));
